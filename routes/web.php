8daac8f614baa2ab0167408eccb9c742abfbcb3b
Mohmed Albakro <mohamadalbakro104@gmial.com>
2026-05-02 15:52:21 +0300

    thio

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 Route::get('test',function (){
- return "  i am in test branch";
+ return " i am in MAIN";
 });
 require __DIR__.'/settings.php';
